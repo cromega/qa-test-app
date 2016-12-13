c11692c6efea2f8388e6144c88360e0f72a9264a
Cristina Iannone <iannonecristina@gmail.com>
2016-12-13 22:20:54 +0000

    'wrong password confirmation' & 'too short username' tests

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -17,12 +17,12 @@ describe "Create a user", driver: :selenium do
 
     expect(page).to have_content ('User has been created successfully!')
 
-    sleep 2
+
   end
 
   context "with an invalid email address" do
     it "displays an error message" do
-      
+
       fill_in('Username', with: 'Sirion')
       fill_in('Email', with: 'siriondragon.gmail.com')
       fill_in('Password', with: 'ilovefire')
@@ -32,5 +32,32 @@ describe "Create a user", driver: :selenium do
       expect(page).to have_content ('Email has to look like an email address')
     end
   end
-  
+
+context "with a wrong password confirmation" do
+    it "displays an error message" do
+
+      fill_in('Username', with: 'Sirion')
+      fill_in('Email', with: 'siriondragon@gmail.com')
+      fill_in('Password', with: 'ilovefire')
+      fill_in('Password confirmation', with: 'ilovewater')
+      click_button('Create')
+
+      expect(page).to have_content ("Password confirmation doesn't match Password")
+    end
+  end
+
+  context "with a too short username" do
+    it "displays an error message" do
+
+      fill_in('Username', with: 'Sir')
+      fill_in('Email', with: 'siriondragon@gmail.com')
+      fill_in('Password', with: 'ilovefire')
+      fill_in('Password confirmation', with: 'ilovewater')
+      click_button('Create')
+
+      expect(page).to have_content ("Username is too short (minimum is 4 characters)")
+      sleep 2
+    end
+  end
+
 end
