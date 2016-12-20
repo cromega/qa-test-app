6c44ddb0bedac9886f889d99f9254f53f72c36db
Bence Monus <crome@moronia.hu>
2016-12-20 00:17:33 +0000

    only validate password if changed
    make tests blow up on invalid updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
 
   validates :username, uniqueness: true, length: { minimum: 4 }
   validates :email, format: { with: /@/, message: "has to look like an email address" }
-  validates :password, confirmation: true, presence: true
-  validates :password_confirmation, presence: true
+  validates :password, confirmation: true, presence: true, if: ->{ password_changed? }
+  validates :password_confirmation, presence: true, if: ->{ password_changed? }
 end
